6d1b39f72882b8bcaf904d43abc89682eb3c5257
Anwesh43 <anweshthecool0@gmail.com>
2019-12-02 05:19:05 +0530

    adding State class

--- Stage.js
+++ Stage.js
@@ -27,3 +27,29 @@ class Stage {
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
     }
 }
+
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += scGap * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.dir
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
